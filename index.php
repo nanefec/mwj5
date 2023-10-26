0921fe1d99793afdd831f50f1fc2228d13a9dbfd
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-10-26 10:19:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,15 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="utf-8">
-	
-	<title>index.html</title>
+    <meta charset="utf-8">
+    <title>Geometrische Figuren</title>
 </head>
 <body>
-	
-	<h1>Geometrische Figuren</h1>
-	
-	<?php
-
-	
-	<ul>
-		<li><a href="kugel.php"></a></li>
-		<li><a href="zylinder.php"></a></li>
-		<li><a href="wuerfel.php"></a></li>
-	</ul>
-	
+    <h1>Geometrische Figuren</h1>
+    <ul>
+        <li><a href="kugel.php">Kugel</a></li>
+        <li><a href="zylinder.php">Zylinder</a></li>
+        <li><a href="wuerfel.php">Würfel</a></li>
+    </ul>
 </body>
 </html>
