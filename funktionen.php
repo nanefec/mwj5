253fd9d31b3725a0f7f244201184af061f55f7e0
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-10-26 10:31:36 +0200

    Update funktionen.php

--- funktionen.php
+++ funktionen.php
@@ -1,37 +1,11 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Funktionen</title>
-</head>
+<?php
 
-<body>
-	<?php
-    // PHP-Funktion zur Berechnung des Volumens einer Kugel
-    function kugelVolumen($radius) {
-        return (4/3) * M_PI * pow($radius, 3);
-    }
+function kugelVolumen($radius) {
+    return (4/3) * M_PI * pow($radius, 3);
+}
 
-    // PHP-Funktion zur Berechnung der Oberfläche einer Kugel
-    function kugelOberflaeche($radius) {
-        return 4 * M_PI * pow($radius, 2);
-    }
 
-    // Formularverarbeitung
-    if (isset($_POST['submit'])) {
-        $radius = floatval($_POST['radius']);
-        
-        if ($radius > 0) {
-            $volumen = kugelVolumen($radius);
-            $oberflaeche = kugelOberflaeche($radius);
-            
-            echo "<p>Radius: $radius</p>";
-            echo "<p>Volumen: $volumen</p>";
-            echo "<p>Oberfläche: $oberflaeche</p>";
-        } else {
-            echo "<p>Der Radius muss größer als 0 sein.</p>";
-        }
-    }
-    ?>
-</body>
-</html>
+function kugelOberflaeche($radius) {
+    return 4 * M_PI * pow($radius, 2);
+}
+?>
