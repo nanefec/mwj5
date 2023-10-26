c0c0439237bd8049ae3761927b3d7c771983bda9
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-10-26 10:31:53 +0200

    Update kugel.php

--- kugel.php
+++ kugel.php
@@ -1,22 +1,41 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Kugelberechnung</title>
+    <meta charset="utf-8">
+    <title>Kugelberechnung</title>
 </head>
-
-	<h1>Kugelberechnung</h1>
-	
-	<form method="post" action="kugel.php">
-	Radius: <input type="text" name="radius"><br>
-	<input type="submit" name="submit" value="Berechnen">
-	</form>
-	
-	<?php
-		include('kugel.php');
-	?>
-	
-	
 <body>
+    <h1>Kugelberechnung</h1>
+    
+    <form method="post" action="">
+        Radius: <input type="text" name="radius"><br>
+        <input type="submit" name="submit" value="Berechnen">
+    </form>
+
+    <?php
+    
+    if (isset($_POST['submit'])) {
+        $radius = floatval($_POST['radius']);
+        
+        if ($radius > 0) {
+            // Einbinden der Berechnungsfunktionen aus funktion.php
+            include('funktionen.php');
+            
+            $volumen = kugelVolumen($radius);
+            $oberflaeche = kugelOberflaeche($radius);
+            
+            echo "<p>Radius: $radius</p>";
+            echo "<p>Volumen: $volumen</p>";
+            echo "<p>Oberfläche: $oberflaeche</p>";
+        } else {
+            echo "<p>Der Radius muss größer als 0 sein.</p>";
+        }
+    }
+    ?>
 </body>
 </html>
+
+
+
+
+
