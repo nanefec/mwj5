a09398d873deb2deb71a8c7fb38afe1043037636
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-10-26 11:15:56 +0200

    Update funktionen.php

--- funktionen.php
+++ funktionen.php
@@ -26,3 +26,22 @@ function wuerfelOberflaecheninhalt($seitenlaenge) {
     return 6 * pow($seitenlaenge, 2);
 }
 ?>
+
+<?php
+// PHP-Funktion zur Berechnung des Volumens eines Zylinders
+function zylinderVolumen($radius, $hoehe) {
+    return M_PI * pow($radius, 2) * $hoehe;
+}
+
+// PHP-Funktion zur Berechnung der Oberfläche eines Zylinders
+function zylinderOberflaeche($radius, $hoehe) {
+    $mantelflaeche = (2 * M_PI * $radius * $hoehe);
+    $oberflaeche = (2 * M_PI * pow($radius, 2) + $mantelflaeche);
+    return $oberflaeche;
+}
+
+// PHP-Funktion zur Berechnung der Lateralflaeche eines Zylinders
+function zylinderLateralflaeche($radius, $hoehe) {
+    return 2 * M_PI * $radius * $hoehe;
+}
+?>
