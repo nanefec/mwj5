55d558f5ad2ca1963854db52c765bf70f80b69cc
leaprwt <116566313+leaprwt@users.noreply.github.com>
2023-10-26 10:52:40 +0200

    Update funktionen.php

--- funktionen.php
+++ funktionen.php
@@ -9,3 +9,20 @@ function kugelOberflaeche($radius) {
     return 4 * M_PI * pow($radius, 2);
 }
 ?>
+
+<?php
+
+function wuerfelVolumen($seitenlaenge) {
+    return pow($seitenlaenge, 3);
+}
+
+
+function wuerfelMantelflaeche($seitenlaenge) {
+    return 6 * pow($seitenlaenge, 2);
+}
+
+
+function wuerfelOberflaecheninhalt($seitenlaenge) {
+    return 6 * pow($seitenlaenge, 2);
+}
+?>
